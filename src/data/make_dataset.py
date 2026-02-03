b7cd8bc7a18939048d468ddc8ef0e3fcdcaace13
timotheemonod-creator <timothee.monod@gmail.com>
2026-02-03 12:06:33 +0100

    First preprocessing of the data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -53,7 +53,7 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
 
     # Merge datasets
     df = merge_datasets(df_users, df_veh, df_places, df_caract)
-
+    """
     # Add new columns
     df = add_new_columns(df, nb_victim, nb_vehicules)
 
@@ -68,7 +68,7 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
 
     # Drop lines with NaN values
     df = drop_lines_with_nan_values(df)
-
+    """
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = split_data(df)
 
